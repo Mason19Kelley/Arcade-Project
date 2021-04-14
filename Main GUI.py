d5efba32c19c04682a97041596746d10d31f4b49
JHartung72 <jordanhartung72@gmail.com>
2021-04-14 12:26:44 -0500

    Background images added and background images implemented into main screen

--- Main GUI.py
+++ Main GUI.py
@@ -3,27 +3,32 @@
 from tkinter import *
 import pyglet
 pyglet.font.add_file('ARCADECLASSIC.TTF')
-class MenuScreen(Frame):
+
+class MenuScreen(Frame, Canvas):
     def __init__(self, container):
         Frame.__init__(self, container)
         self.menuSet()
     def menuSet(self):
-        self.display = Label(self, text = "Memory", font =("ARCADECLASSIC", 25), height = 2, width = 15, anchor = 'center', bg = "white")
+        bg = PhotoImage(file="menuIMG/mario.gif")
+        back = Label(self, width = WIDTH, height = HEIGHT, image = bg)
+        back.image = bg
+        back.place(x = 0, y= 0, relwidth = 1, relheight = 1)
+        self.display = Label(self, text = "Memory", font =("ARCADECLASSIC", 25), height = 2, width = 15, anchor = 'center')
         self.display.grid(row = 0, column = 0)
-        self.display = Label(self, text = "Pong", font =("ARCADECLASSIC", 25), height = 2, width = 15, anchor = 'center', bg = "white")
+        self.display = Label(self, text = "Pong", font =("ARCADECLASSIC", 25), height = 2, width = 15, anchor = 'center')
         self.display.grid(row = 0, column = 1)
-        self.display = Label(self, text = "Speed", font =("ARCADECLASSIC", 25), height = 2, width = 15, anchor = 'center', bg = "white")
+        self.display = Label(self, text = "Speed", font =("ARCADECLASSIC", 25), height = 2, width = 15, anchor = 'center')
         self.display.grid(row = 0, column = 2)
         img = PhotoImage(file = "menuIMG/MEMORY.gif")
-        self.display = Label(self, image = img, height = 245, width = 272, anchor = 'center', bg = "white")
+        self.display = Label(self, image = img, height = 245, width = 272, anchor = 'center', bg = 'deep sky blue')
         self.display.image = img
         self.display.grid(row = 1, column = 0)
         img = PhotoImage(file = "menuIMG/PONG.gif")
-        self.display = Label(self, image = img, height = 245, width = 272, anchor = 'center', bg = "white")
+        self.display = Label(self, image = img, height = 245, width = 272, anchor = 'center', bg = 'deep sky blue')
         self.display.image = img
         self.display.grid(row = 1, column = 1)
         img = PhotoImage(file = "menuIMG/lpr.gif")
-        self.display = Label(self, image = img , height = 245, width = 272, anchor = 'center', bg = "white")
+        self.display = Label(self, image = img , height = 245, width = 272, anchor = 'center', bg = 'deep sky blue')
         self.display.image = img
         self.display.grid(row = 1, column = 2)
         button = Button(self, text = "Start", font = ('ARCADECLASSIC', 15), height = 2, width = 15, anchor = 'center', bg = 'white')
@@ -34,11 +39,14 @@ class MenuScreen(Frame):
         button.grid(row = 3, column = 1)
         button = Button(self, text = "Start", font = ('ARCADECLASSIC', 15), height = 2, width = 15, anchor = 'center', bg = 'white')
         button.grid(row = 2, column = 2)
-        self.pack()
+        button = Button(self, text = "Leaderboards", font = ('ARCADECLASSIC', 15), height = 2, width = 15, anchor = 'center', bg = 'light grey')
+        button.grid(row = 4)
+        self.pack(fill = "both", expand = True)
 window = Tk()
 window.title("Arcade")
 WIDTH = 800
 HEIGHT = 480
 window.geometry("{}x{}".format(WIDTH, HEIGHT))
 a1 = MenuScreen(window)
+
 window.mainloop()
